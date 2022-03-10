1d1892fabdb4db402ed6ef4fc10abbee791acadc
Fiqahhassan <fiqah.hjhassan@gmail.com>
2022-03-10 12:54:45 +0800

    Lesson 1.3 - Title, Score and Previous Score Details

--- project01/BabyBirdGame.java
+++ project01/BabyBirdGame.java
@@ -3,51 +3,71 @@ package project01;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class BabyBirdGame extends Application {
 	private static Stage primaryStage;
 
-	@Override
-	public void start(Stage arg0) throws Exception {	
-		primaryStage = arg0;
-		initGUI();
+	private static int score = 0;
+	private static int previousScore = 0;
 
-		arg0.setTitle("Baby Bird");
-		arg0.setResizable(false);
-		arg0.sizeToScene();
-		arg0.show();
+		@Override
+		public void start(Stage arg0) throws Exception {	
+			primaryStage = arg0;
+			initGUI();
+
+			arg0.setTitle("Baby Bird");
+			arg0.setResizable(false);
+			arg0.sizeToScene();
+			arg0.show();
 
 
+		}
+
+		public static void initGUI() {
+			VBox rootPane = new VBox();
+			rootPane.setAlignment(Pos.TOP_CENTER);
+			rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
+					null, null)));
+			
+			previousScore = score;
+			score = 0;
+
+			// Title
+			Label titleLabel = new Label("Baby Bird");
+			titleLabel.setFont(Font.font(32));
+			titleLabel.setTextFill(Color.WHITE);
+
+			// Score
+			ScorePane scorePane = new ScorePane("Score: ", score,
+					 Color.DARKBLUE, Color.WHITE);
+
+			// Previous Score
+			ScorePane previousScorePane = new ScorePane("Previous Score: ",
+					previousScore,
+					 Color.DARKCYAN, Color.WHITE);
+
+			// Bird Flight
+
+			// Help Message
+
+
+			rootPane.getChildren().addAll(titleLabel, scorePane,
+					previousScorePane);
+
+			Scene scene = new Scene(rootPane);
+
+			primaryStage.setScene(scene);
+
+		}
+
+		public static void main(String[] args) {
+			Application.launch(args);
+		}
 	}
-
-	public static void initGUI() {
-		VBox rootPane = new VBox();
-		rootPane.setAlignment(Pos.TOP_CENTER);
-		rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
-				null, null)));
-
-		// Title
-
-		// Score
-
-		// Previous Score
-
-		// Bird Flight
-
-		// Help Message
-
-		Scene scene = new Scene(rootPane);
-
-		primaryStage.setScene(scene);
-
-	}
-
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-}
