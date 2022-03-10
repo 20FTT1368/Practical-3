a2074d8d7cad4c46063b38d99f9d0e4865d3c98d
Fiqahhassan <fiqah.hjhassan@gmail.com>
2022-03-10 13:08:58 +0800

    Lesson 1.4 - Flight Pane

--- project01/BabyBirdGame.java
+++ project01/BabyBirdGame.java
@@ -17,57 +17,58 @@ public class BabyBirdGame extends Application {
 	private static int score = 0;
 	private static int previousScore = 0;
 
-		@Override
-		public void start(Stage arg0) throws Exception {	
-			primaryStage = arg0;
-			initGUI();
+	@Override
+	public void start(Stage arg0) throws Exception {	
+		primaryStage = arg0;
+		initGUI();
 
-			arg0.setTitle("Baby Bird");
-			arg0.setResizable(false);
-			arg0.sizeToScene();
-			arg0.show();
-
-
-		}
-
-		public static void initGUI() {
-			VBox rootPane = new VBox();
-			rootPane.setAlignment(Pos.TOP_CENTER);
-			rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
-					null, null)));
-			
-			previousScore = score;
-			score = 0;
-
-			// Title
-			Label titleLabel = new Label("Baby Bird");
-			titleLabel.setFont(Font.font(32));
-			titleLabel.setTextFill(Color.WHITE);
-
-			// Score
-			ScorePane scorePane = new ScorePane("Score: ", score,
-					 Color.DARKBLUE, Color.WHITE);
-
-			// Previous Score
-			ScorePane previousScorePane = new ScorePane("Previous Score: ",
-					previousScore,
-					 Color.DARKCYAN, Color.WHITE);
-
-			// Bird Flight
-
-			// Help Message
+		arg0.setTitle("Baby Bird");
+		arg0.setResizable(false);
+		arg0.sizeToScene();
+		arg0.show();
 
 
-			rootPane.getChildren().addAll(titleLabel, scorePane,
-					previousScorePane);
-
-			Scene scene = new Scene(rootPane);
-
-			primaryStage.setScene(scene);
-
-		}
-
-		public static void main(String[] args) {
-			Application.launch(args);
-		}
 	}
+
+	public static void initGUI() {
+		VBox rootPane = new VBox();
+		rootPane.setAlignment(Pos.TOP_CENTER);
+		rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
+				null, null)));
+
+		previousScore = score;
+		score = 0;
+
+		// Title
+		Label titleLabel = new Label("Baby Bird");
+		titleLabel.setFont(Font.font(32));
+		titleLabel.setTextFill(Color.WHITE);
+
+		// Score
+		ScorePane scorePane = new ScorePane("Score: ", score,
+				Color.DARKBLUE, Color.WHITE);
+
+		// Previous Score
+		ScorePane previousScorePane = new ScorePane("Previous Score: ",
+				previousScore,
+				Color.DARKCYAN, Color.WHITE);
+
+		// Bird Flight
+		FlightPane flightPane = new FlightPane();
+
+		// Help Message
+
+
+		rootPane.getChildren().addAll(titleLabel, scorePane, previousScorePane,
+				flightPane);
+
+		Scene scene = new Scene(rootPane);
+
+		primaryStage.setScene(scene);
+
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
